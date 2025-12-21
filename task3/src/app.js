a23fdbf7ad4374b2d4240f15525160ad53120479
Anteneh Wondwosen <antenehwondwosen@gmail.com>
2025-12-21 08:18:10 +0200

    included errorhandler

--- task3/src/app.js
+++ task3/src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
 import bookRoute from './route/bookRoute.js';
-
+import errorHandler from './utils/errorHandler.js';
 const app = express ();
 
 app.use (express.json ());
-
+app.use (morgan ());
 // Routes
 app.use ('/books', bookRoute);
-
+app.use ((req, res, next) => {
+  res.status (404).json ({
+    success: false,
+    message: 'Route not found',
+  });
+});
+app.use (errorHandler);
 export default app;
